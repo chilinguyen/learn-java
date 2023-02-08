00c0576a6f56051f5925fee6a749c18666cbd64d
chilinguyen <chilinguyen.tch@gmail.com>
2023-02-08 10:32:28 +0700

    first commit

---
+++ src/main/java/com/learnjava/LearnjavaApplication.java
@@ -0,0 +1,15 @@
+package com.learnjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+@SpringBootApplication(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
+public class LearnjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnjavaApplication.class, args);
+    }
+
+}
